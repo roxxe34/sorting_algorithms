12a35f92232522cbf0e58d0e89b4e3516be23bed
roxxe34 <hamzafarissi7@gmail.com>
2023-09-23 22:08:21 +0100

    done

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,20 +23,22 @@ void swap(int *a, int *b)
  */
 void selection_sort(int *array, size_t size)
 {
-	int min;
+	int *min;
 	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = i;
-	for (j = i + 1 ; j < size; j++)
-	{
-		if (array[j] < array[min])
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
 		{
-			min = j;
+			swap(array + i, min);
+			print_array(array, size);
 		}
-	}
-	swap(&array[i], &array[min]);
-	print_array(array, size);
 	}
 }
